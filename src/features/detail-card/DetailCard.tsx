c0553e9db263f81ed54d76d2c5eb8707fd382274
Loic <loic.ronzier@etu.univ-lyon1.fr>
2018-12-10 14:16:47 +0100

    [FEAT] add feature to project

---
+++ src/features/detail-card/DetailCard.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+class DetailCard extends React.Component {
+
+  public render(): any {
+    return <div><p>Hello World</p></div>;
+  }
+}
+
+export default DetailCard;
