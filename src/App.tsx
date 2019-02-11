5c489590865443d5c2c375a0d713727efe2bb211
theo <theo.tanchoux@etu.univ-lyon1.fr>
2019-02-11 11:54:30 +0100

    [FEAT] Add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
-import { Route , Switch } from "react-router-dom";
+import { Navbar } from "react-bootstrap";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import ListCards from './features/list-cards/ListCards';
 import ListDeck from "./features/list-decks/ListDeck";
@@ -7,10 +8,15 @@ import ListDeck from "./features/list-decks/ListDeck";
 class App extends React.Component {
   public render() {
     return (
-      <Switch>
-        <Route exact path="/" component={ListDeck} />
-        <Route path="/card" component={ListCards} />
-      </Switch>
+      <div>
+        <Navbar bg="navbar" variant="dark">
+          <Navbar.Brand href="/">PokecardAppReact</Navbar.Brand>
+        </Navbar>
+        <Switch>
+          <Route exact path="/" component={ListDeck} />
+          <Route path="/card" component={ListCards} />
+        </Switch>
+      </div>
     );
   }
 }
