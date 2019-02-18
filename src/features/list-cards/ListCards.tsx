4acf830309d0902d414d28f5c286eb0e25644dcf
theo <theo.tanchoux@etu.univ-lyon1.fr>
2019-02-18 10:11:37 +0100

    [FEAT] search bar cards

--- src/features/list-cards/ListCards.tsx
+++ src/features/list-cards/ListCards.tsx
@@ -1,9 +1,14 @@
 import * as React from "react";
+import { FaSearch } from "react-icons/fa";
+import { ClipLoader } from "react-spinners";
+import { ICards } from "../../model/Cards";
 import "./list-cards.css";
 
 interface IMyState {
   codeSets: string;
-  listCards: any[];
+  listCards: ICards[];
+  listSearch: ICards[];
+  showSpinner: boolean;
 }
 
 class ListCards extends React.Component<{}, IMyState> {
@@ -16,8 +21,10 @@ class ListCards extends React.Component<{}, IMyState> {
     this.state = {
       codeSets: params.get("setCode") ? params.get("setCode")! : "",
       listCards: [],
+      listSearch: [],
+      showSpinner: true
     };
-       
+
   }
 
   public componentDidMount() {
@@ -27,15 +34,51 @@ class ListCards extends React.Component<{}, IMyState> {
   public render(): any {
     return (
       <div className="container">
-        {this.state.listCards.map(res => {
-          return (
-            <div className="cellule" key={res.id}>
-              <img src={res.imageUrl} />
-            </div>
-          );
-        })}
+        <div className="search-form">
+          <FaSearch className="search-button" />
+          <input
+            type="text"
+            className="search-input"
+            placeholder="Recherche"
+            onChange={e => this.change(e)}
+          />
+        </div>
+        {this.state.listSearch.length > 0 ? (
+          this.state.listSearch.map(res => {
+            return (
+              <div className="cellule" key={res.id}>
+                <img src={res.imageUrl} />
+              </div>
+            );
+          })
+        ) : this.state.showSpinner === false ? (
+          <div className="empty"> Aucun élément trouvé </div>
+        ) : null}
+
+        <div className="spinner">
+          <ClipLoader
+            sizeUnit={"px"}
+            size={150}
+            color={"#123abc"}
+            loading={this.state.showSpinner}
+          />
+        </div>
       </div>
     );
+  }
+
+  private change(event: React.ChangeEvent<HTMLInputElement>): void {
+    console.log(event.currentTarget.value);
+
+    const listFilter: ICards[] = [];
+    this.state.listCards.map(element => {
+      const name = element.name.toLowerCase();
+      console.log(element.name);
+      if (name.includes(event.currentTarget.value.toLowerCase())) {
+        listFilter.push(element);
+      }
+    });
+    this.setState({ listSearch: listFilter });
   }
 
   private getCardsBySets(): void {
@@ -44,7 +87,11 @@ class ListCards extends React.Component<{}, IMyState> {
       .then(results => results.json())
       .then(data => {
         console.log(data.cards.length)
-        this.setState({ listCards: data.cards});
+        this.setState({
+          listCards: data.cards,
+          listSearch: data.cards,
+          showSpinner: false
+        });
       });
   }
 }
