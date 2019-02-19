a80fa07a1b700eeb14a86e5f01cd10e5b8432ba8
Loic <loic.ronzier@etu.univ-lyon1.fr>
2019-02-19 13:06:44 +0100

    [FEAT] search and infinite scroll

--- src/features/list-cards/ListCards.tsx
+++ src/features/list-cards/ListCards.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { FaTimes } from "react-icons/fa";
-import { ClipLoader } from "react-spinners";
+import { ClipLoader, PulseLoader } from "react-spinners";
 import Waypoint from "react-waypoint";
 import { ICards } from "../../model/Cards";
 import { SearchBar } from "../components/search-bar/SearchBar";
@@ -9,12 +9,12 @@ import "./list-cards.css";
 interface IMyState {
   codeSets: string;
   listCards: ICards[];
-  listSearch: ICards[];
   showSpinner: boolean;
   focus: string | null;
   currentPage: number;
   maxPage: number;
   showLoading: boolean;
+  currentName: string;
 }
 
 class ListCards extends React.Component<{}, IMyState> {
@@ -26,10 +26,10 @@ class ListCards extends React.Component<{}, IMyState> {
 
     this.state = {
       codeSets: params.get("setCode") ? params.get("setCode")! : "",
+      currentName: "",
       currentPage: 1,
       focus: null,
       listCards: [],
-      listSearch: [],
       maxPage: 0,
       showLoading: false,
       showSpinner: true
@@ -49,24 +49,23 @@ class ListCards extends React.Component<{}, IMyState> {
   public fetchMore(): void {
     if (!this.state.showSpinner) {
       if (this.state.currentPage + 1 <= this.state.maxPage) {
-      
         this.setState({ showLoading: true });
         const url = `${ListCards.API_URL_CARDS}?setCode=${
           this.state.codeSets
-        }&pageSize=20&page=${this.state.currentPage + 1}`;
-        console.log(url)
+        }&pageSize=20&page=${this.state.currentPage + 1}&name=${this.state.currentName}`;
+        console.log(url);
         fetch(url)
           .then(results => {
             return results.json();
           })
           .then(data => {
-            this.state.listSearch.push(data)
-            this.state.listCards.push(data)
-            this.setState({
-              currentPage: this.state.currentPage+1,
+            this.setState(state => ({
+              currentPage: state.currentPage + 1,
+              listCards: [...state.listCards, ...data.cards],
               showLoading: false,
-              showSpinner: false,
-            });
+              showSpinner: false
+            }));
+            console.log(this.state);
           });
       }
     }
@@ -76,10 +75,10 @@ class ListCards extends React.Component<{}, IMyState> {
     return (
       <div className="container">
         <SearchBar onChange={(e: string) => this.change(e)} />
-        {this.state.listSearch.length > 0 ? (
-          this.state.listSearch.map(res => {
+        {this.state.listCards.length > 0 ? (
+          this.state.listCards.map(res => {
             return (
-              <div className="cellule" key={Math.random()}>
+              <div className="cellule" key={res.id}>
                 <img
                   src={res.imageUrl}
                   onClick={() => {
@@ -110,7 +109,12 @@ class ListCards extends React.Component<{}, IMyState> {
           </div>
         ) : null}
         <Waypoint onEnter={() => this.fetchMore()} />
-
+        <PulseLoader
+            sizeUnit={"px"}
+            size={30}
+            color={"#123abc"}
+            loading={this.state.showLoading}
+          />
         <div className="spinner">
           <ClipLoader
             sizeUnit={"px"}
@@ -124,14 +128,35 @@ class ListCards extends React.Component<{}, IMyState> {
   }
 
   private change(event: string): void {
-    const listFilter: ICards[] = [];
-    this.state.listCards.map(element => {
-      const name = element.name.toLowerCase();
-      if (name.includes(event.toLowerCase())) {
-        listFilter.push(element);
-      }
-    });
-    this.setState({ listSearch: listFilter });
+    this.setState({currentName: event})
+    this.getCardsByName(event)
+  }
+
+  private getCardsByName(name: string): void {
+    let pageSize = 0;
+    let totalCard = 0;
+    const url = `${ListCards.API_URL_CARDS}?setCode=${
+      this.state.codeSets
+    }&pageSize=20&name=${name}`;
+    fetch(url)
+      .then(results => {
+        results.headers.forEach((val, key) => {
+          if (key === "page-size") {
+            pageSize = Number(val);
+          } else if (key === "total-count") {
+            totalCard = Number(val);
+          }
+        });
+        return results.json();
+      })
+      .then(data => {
+        this.setState({
+          currentPage: 1,
+          listCards: data.cards,
+          maxPage: Math.round(totalCard / pageSize),
+          showSpinner: false
+        });
+      });
   }
 
   private getCardsBySets(): void {
@@ -154,7 +179,6 @@ class ListCards extends React.Component<{}, IMyState> {
       .then(data => {
         this.setState({
           listCards: data.cards,
-          listSearch: data.cards,
           maxPage: Math.round(totalCard / pageSize),
           showSpinner: false
         });
